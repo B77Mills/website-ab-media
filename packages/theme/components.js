47f9931efbecc50615f5c6bbc7c6000c32d9efb6
Brian Miller <brian@parameter1.com>
2021-06-18 13:12:34 -0500

    Remove theme package

--- packages/theme/components.js
+++
@@ -1,7 +0,0 @@
-const ThemeLatestContentListFlow = require('./components/flows/latest-content-list');
-const ThemeMagazineIssueArchiveFlow = require('./components/flows/magazine-issue-archive');
-
-module.exports = {
-  ThemeLatestContentListFlow,
-  ThemeMagazineIssueArchiveFlow,
-};
--- packages/theme/fragments.js
+++
@@ -1,9 +0,0 @@
-const contentList = require('./graphql/fragments/content-list');
-const contentLatest = require('./graphql/fragments/content-latest');
-const magazineIssueArchive = require('./graphql/fragments/magazine-issue-archive');
-
-module.exports = {
-  contentList,
-  contentLatest,
-  magazineIssueArchive,
-};
